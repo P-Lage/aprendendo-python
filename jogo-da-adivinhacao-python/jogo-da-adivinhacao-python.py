4a063c431bae195d85d7f1cbc56a07f11c7afb79
Pedro Lage <lagecomg@gmail.com>
2025-02-11 19:14:37 -0300

    Capturar a entrada do usuário

--- jogo-da-adivinhacao-python/jogo-da-adivinhacao-python.py
+++ jogo-da-adivinhacao-python/jogo-da-adivinhacao-python.py
@@ -1,3 +1,8 @@
 # Definição lógica do jogo da adivinhação
 
 numero_secreto = 7
+
+# Capturando a entrada do usuário
+
+palpite = int(input("Digite seu palpite: "))
+print(f"Você digitou: {palpite}")
