fac833a1967fed4d4b7c5f6e6c8a7504e2780a4b
Pedro Lage <lagecomg@gmail.com>
2025-02-11 19:26:55 -0300

    Implementar laço de repetição com entrada e erro:entrada inválida

--- jogo-da-adivinhacao-python/jogo-da-adivinhacao-python.py
+++ jogo-da-adivinhacao-python/jogo-da-adivinhacao-python.py
@@ -2,9 +2,16 @@
 
 def main():
     # Definição da lógica do jogo
-    
     numero_secreto = 7
     tentativas = 0
+
+    # Laço de repetição contendo o jogo
+    while True:
+        try:
+            palpite = int(input("Digite o seu palpite: "))
+        except ValueError:
+            print("Palpite inválido! Digite apenas números inteiros.")
+            continue
 
 if __name__ == "__main__":
     main()
