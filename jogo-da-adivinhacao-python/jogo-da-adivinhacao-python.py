b3cc9af95da335cb9055a6b261c9a4124b94db8f
Pedro Lage <lagecomg@gmail.com>
2025-02-11 19:18:20 -0300

    Aplicar uma estrutura de repetição para checagem do palpite

--- jogo-da-adivinhacao-python/jogo-da-adivinhacao-python.py
+++ jogo-da-adivinhacao-python/jogo-da-adivinhacao-python.py
@@ -7,11 +7,14 @@ numero_secreto = 7
 palpite = int(input("Digite seu palpite: "))
 print(f"Você digitou: {palpite}")
 
-# Comparando e estruturando a decisão
+# Estrutura de repetição para checagem do palpite
 
-if palpite > numero_secreto:
-    print("Você errou! O seu palpite foi maior que o número secreto.")
-elif palpite < numero_secreto:
-    print("Você errou! O seu palpite foi menor que o número secreto.")
-else:
-    print("Parabéns! Você acertou o número secreto.")
+while palpite != numero_secreto:
+    if palpite > numero_secreto:
+        print("Você errou! O seu palpite foi maior que o número secreto.")
+    elif palpite < numero_secreto:
+        print("Você errou! O seu palpite foi menor que o número secreto.")
+
+    palpite = int(input("Digite seu palpite: "))
+
+print("Parabéns! Você acertou o número secreto.")
